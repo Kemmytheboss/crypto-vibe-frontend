2e5bb9e50bd0a216daceecee97ab622cdccb4dd6
Verah Mokaya <verahmokaya475@gmail.com>
2025-10-21 17:06:38 +0300

    added about link to show details about crypto app

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>About Crypto Tracker</h2>
+      <p>This app allows you to track cryptocurrencies and add your favorite ones.</p>
+    </div>
+  );
+}
+
+export default About;
